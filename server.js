6572ade917b4855e0db285c3a242fc5f43e51a51
Schaddai04 <111158613+Schaddai04@users.noreply.github.com>
2022-11-18 21:49:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at http://${port}/`);
 });
