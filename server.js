3b317ef6da098c18c70cc581722e3c48352f44e4
Schaddai04 <111158613+Schaddai04@users.noreply.github.com>
2022-11-18 21:47:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plan');
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
-server.listen (port, () => { 
-  console.log('Server running on port: ${port}');
-}
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
